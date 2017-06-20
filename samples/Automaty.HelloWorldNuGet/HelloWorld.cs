6e88385258dca9642745c782a6ae1d2c5afeba05
Christopher Dresel <christopher.dresel@spatial-focus.net>
2017-06-20 12:20:07 +0200

    Added HelloWorldNuGet sample.

---
+++ samples/Automaty.HelloWorldNuGet/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿namespace Automaty.HelloWorldNuGet
+{
+	using System.IO;
+	using System.Threading.Tasks;
+	using Automaty.Common.Execution;
+	using Automaty.Common.Output;
+	using Newtonsoft.Json;
+
+	public class HelloWorld : IAutomatyHost
+	{
+		public void Execute(IScriptContext context)
+		{
+			context.Output["helloworld.json"].WriteLine(JsonConvert.SerializeObject("Hello World!"));
+		}
+	}
+}
